995636de9373399469b6880a64c692adbcd44120
work <work@Erriott.local>
2020-03-10 16:13:48 +0530

    upda

--- js/pfitmpl20021000.js
+++ js/pfitmpl20021000.js
@@ -574,7 +574,7 @@ function createShopMM() {
 function createLookMM() {
     var col1 = createDropdownColumn([lotm, ramp], "col-6");
     var col2 = createDropdownColumn([moods], "col-6");
-    return createMinWidthDDCard([col1, col2], "350px", false);
+    return createMinWidthDDCard([col1, col2], "330px", false);
 }
 
 function createBlogDD () {
