e0eed7d3064d8d2b7e38c06908b83e3a95c3498f
work <work@Erriott.local>
2020-03-10 12:46:13 +0530

    update

--- js/pfitmpl20021000.js
+++ js/pfitmpl20021000.js
@@ -411,7 +411,7 @@ var siteMap = [
                 url: "/about.html"
             },
             {
-                title: "Look!",
+                title: "Look",
                 url: "/look.html"
             }
         ]
