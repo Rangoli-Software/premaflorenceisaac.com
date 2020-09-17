27db3f63934312cba7ced22284e5029e52380eb8
am <am@Erriott.local>
2020-09-17 17:13:59 +0530

    update summer dreams

--- js/pfinight20091700.js
+++ js/pfinight20091700.js
@@ -160,9 +160,8 @@ const celaData = {
         }
     ],
     sizes: ['S', 'M', 'L', 'XL'],
-    description: '',
-    garmentDetails: ''
-
+    description: 'Celia Skirt is one of our favourite silhouettes. It\'s cut in the finest cotton P.F.I. Tangail. This exquisite cotton is often mistaken as silk by my clients. It has an incredible volume to play around in and is freesize! The waist band is made up of 9 rows of elastic. Because of the need for care while working with the delicate Tangail fabric, it can take a skilled tailor almost a full day of work to create this waistband. The result is a freesize waist with a snug, comfortable fit. It can work from day to night and it\'s groovy flare will have you swirling all day. Pair it with our Portia Top to take it up a notch or wear a basic t-shirt to keep it subtle.',
+    garmentDetails: '<li>Freesize - Broad elastic at the waist</li><li>P.F.I. Tangail</li><li>Midi length</li><li>A line with voluminous flare</li>'
 };
 
 const cleoData = {
@@ -214,8 +213,8 @@ const cleoData = {
         }
     ],
     sizes: ['S', 'M', 'L', 'XL'],
-    description: '',
-    garmentDetails: ''
+    description: 'The Cleopatra Top is cut in ivory ahimsa silk and lined with cotton voile. The round neck is highlighted with black and silver silk P.F.I. Tangail border. It is sleeveless and adds a comfortable chic element to your outfit. Pair it with our Cleopatra Shorts or skirts to keep it easy.',
+    garmentDetails: '<li>Round neck</li><li>Sleeveless</li><li>Short length</li><li>P.F.I. Tangail border at the neckline</li><li>Slips on</li><li>Cotton lining</li>'
 
 };
 
@@ -268,8 +267,8 @@ const emlaData = {
         }
     ],
     sizes: ['S', 'M', 'L', 'XL'],
-    description: '',
-    garmentDetails: ''
+    description: 'Emilia Dress combines traditional handwoven P.F.I. Tangail cotton of Bengal with a western off-shoulder detail. The bias cut of the fabric goes around to overlap in uneven hemline. This gives the dress a voluminous flare which mimics your movements cheerfully. This dress stays true to a neutral palette of black with a metallic touch of tiny silver motifs.',
+    garmentDetails: '<li>Off-shoulder</li><li>Half sleeves</li><li>Zip at the back</li><li>A line with voluminous flare</li>'
 
 };
 
@@ -322,9 +321,8 @@ const hlnaData = {
         }
     ],
     sizes: ['S', 'M', 'L', 'XL'],
-    description: '',
-    garmentDetails: ''
-
+    description: 'The high-low hemline with voluminous flare gives a flattering flow to the Helena dress. It\'s cut in P.F.I. Tangail cotton in grey stripes featuring an origami-ed silver P.F.I. Tangail border in the front. The contrasting red lining in voile lifts up the muted colour palette. A subtle detail of origami-ed silver P.F.I. Tangail border on the lining of the dress adds to the elegance. The hem is finished with cascading ruffles to add more playfulness to the A line dress.',
+    garmentDetails: '<li>Round neck</li><li>Sleeveless</li><li>High-low hemline</li><li>P.F.I Tangail border in the front</li><li>Contrasting lining in voile</li><li>Zip at the back</li><li>A line with voluminous flare</li>'
 };
 
 const jlitData = {
@@ -376,9 +374,8 @@ const jlitData = {
         }
     ],
     sizes: ['S', 'M', 'L', 'XL'],
-    description: '',
-    garmentDetails: ''
-
+    description: 'The asymmetrical panels are collaged in ivory ahimsa silk, silver P.F.I. Tangail borders and black P.F.I. Tangail cotton with tiny silver motifs. The buttons in the front make it easier to slip into this modest round neck dress with cap sleeves. Pair it with black or silver high heels and enjoy your formal dinners.',
+    garmentDetails: '<li>Round neck</li><li>Cap lseeves</li><li>Asymmetrical</li><li>A line with flare</li><li>Buttons in the front</li>'
 };
 
 const nrsaData = {
@@ -484,9 +481,8 @@ const ophlData = {
         }
     ],
     sizes: ['S', 'M', 'L', 'XL'],
-    description: '',
-    garmentDetails: ''
-
+    description: 'Ophelia Dress is cut in black P.F.I. Tangail cotton with a silver silk panel that highlights the neckline. Its lining extends the hemline to add more flounce to  this jovial dress. The square armhole adds a twist to the modest round neck and the zip in the back makes it easy to slip into.',
+    garmentDetails: '<li>Round neck</li><li>Sleeveless</li><li>A line with voluminous flare</li><li>Zip at the back</li><li>Lining in voile</li>'
 };
 
 const prtaData = {
@@ -538,8 +534,8 @@ const prtaData = {
         }
     ],
     sizes: ['S', 'M', 'L', 'XL'],
-    description: '',
-    garmentDetails: ''
+    description: 'The black and silver silk P.F.I. Tangail panel gives the Portia Top a festive touch. It\'s cut in breathable P.F.I. Tangail cotton which is often mistaken as silk by my clients. It is sleeveless with a modest round neck and has a zip in the back to slip on easily. Pair it with our Celia Skirt for your formal dinners or basic trousers, its versatality will make you wear it more often.',
+    garmentDetails: '<li>Round neck</li><li>Sleeveless</li><li>Zip at the back</li><li>P.F.I. Tangail panel in the front</li>'
 
 };
 
@@ -592,9 +588,8 @@ const tmraData = {
         }
     ],
     sizes: ['S', 'M', 'L', 'XL'],
-    description: '',
-    garmentDetails: ''
-
+    description: 'Tamora Dress is cut in a superior jersey to give a flattering shape. The jersey is draped and twisted to support your bust which will help you go bra-free. The deep V neck gives an illusion of a slender frame. The neck and hemline are accentuated with black and silver silk P.F.I. Tangail border that adds a subtle charm to the black dress.',
+    garmentDetails: '<li>Floor length</li><li>V neck</li><li>Deep V neckline at the back</li><li>Sleeveless</li><li>Gathers in the front</li><li>A line</li><li>P.F.I Tangail border at the neckline and hem</li>'
 };
 
 const ttnaData = {
@@ -646,9 +641,8 @@ const ttnaData = {
         }
     ],
     sizes: ['S', 'M', 'L', 'XL'],
-    description: '',
-    garmentDetails: ''
-
+    description: 'A superior quality jersey is used to drape this little black dress that will hug and flatter every shape. The jersey is gathered in the centre front and back and descends at the sideseam. The shoulder straps in black and silver silk P.F.I. Tangail can be worn on either sides or on the same side to make it an off-shoulder dress. ',
+    garmentDetails: '<li>Draped dress</li><li>V neck</li><li>Sleeveless</li><li>P.F.I. Tangail border at the neckline</li>'
 };
 
 function createBiancaDressJSON() {
@@ -785,12 +779,12 @@ function createProductDB() {
 
 const sd_catalog = {
     skus: ['BNCJKT1505PT','BNCADR1505PT','NKSHMD1501PT','CLPTRT1505PT','CLPTRS1505PT','EMLDRS1505PT','HLNDRS1505PT','JLTDRS1505PT','NRSSTP1505PT','NRSPNT1505PT','OPHLDR1505PT','PRTATP1505PT','BKLLTS1505Je','TTNDRS1505Je'],
-    dresses: ['BNCADR1505PT','EMLDRS1505PT','HLNDRS1505PT','JLTDRS1505PT','OPHLDR1505PT','BKLLTS1505Je','TTNDRS1505Je'],
-    tops: ['CLPTRT1505PT','NRSSTP1505PT','PRTATP1505PT'],
-    pants: ['NRSPNT1505PT'],
+    dresses: ['EMLDRS1505PT','HLNDRS1505PT','JLTDRS1505PT','OPHLDR1505PT','BKLLTS1505Je','TTNDRS1505Je'],
+    tops: ['CLPTRT1505PT','PRTATP1505PT'],
+//    pants: ['NRSPNT1505PT'],
     culottes: ['CLPTRS1505PT'],
     skirts: ['NKSHMD1501PT'],
-    overtops: ['BNCJKT1505PT'],
+//    overtops: ['BNCJKT1505PT'],
     getCategory: function (sku) {
         if (this.dresses.includes(sku)) {
             return "dresses";
@@ -798,18 +792,22 @@ const sd_catalog = {
         if (this.tops.includes(sku)) {
             return "tops";
         }
+/*
         if (this.pants.includes(sku)) {
             return "pants";
         }
+*/
         if (this.culottes.includes(sku)) {
             return "culottes";
         }
         if (this.skirts.includes(sku)) {
             return "skirts";
         }
+/*
         if (this.overtops.includes(sku)) {
             return "overtops";
         }
+*/
         return null;
     },
     productDB: createProductDB(),
