c4e033bd5955604df16707b8d9d3e351f665f13c
am <am@Erriott.local>
2020-12-11 16:15:56 +0530

    adde offer handler

---
+++ netlify-functions/offer.js
@@ -0,0 +1,31 @@
+const promos = {
+	'NEWPROMO': {
+		code: 'NEWPROMO',
+		discountPercentage: 20
+	}
+};
+
+exports.handler = async (event, context, callback) => {
+	const payload = JSON.parse(event.body);
+	const promoKey = payload.promoKey;
+
+	try {
+		const promo = promos[promoKey];
+		if (promo === undefined || promo === null) {
+			return {
+				statusCode: 200,
+				body: null
+			}
+		}
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify(promo)
+		}
+	} catch (e) {
+		return {
+			statusCode: e.code,
+			body: e.message
+		}
+	}
+};
