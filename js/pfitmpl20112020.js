41e1c3a25543dc1d9a81e700c9bac07f96472129
am <am@Erriott.local>
2020-12-12 16:15:30 +0530

    added merchandising images for seragu

--- js/pfitmpl20112020.js
+++ js/pfitmpl20112020.js
@@ -504,6 +504,11 @@ function getSubEntry(item, url) {
 
 const merchInfo = [
     {
+        SKU: 'OVTPSH1501Pa',
+        ledes: [''],
+        images: [{url: '/g/merch/sovr1.jpg'},{url: '/g/merch/sovr2.jpg'}]
+    },
+    {
         SKU: 'OVTPLO1501Pa',
         ledes: [''],
         images: [{url: '/blog/FlorDuomo.jpg'},{url: '/g/merch/lovr1.jpg'},{url: '/g/merch/lovr2.jpg'}]
